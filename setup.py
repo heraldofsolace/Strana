5d69b8ad4a5b52c20e7dd082e4f4eb2c6ab24246
Aniket Bhattacharyea <aniketmail669@gmail.com>
2017-11-28 17:58:46 +0530

    Added setup.py
    Signed-off-by: Aniket Bhattacharyea <aniketmail669@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PEng',
+    version='1.0',
+    packages=['PEng'],
+    url='peng.github.io',
+    license='GNU General Public License v3',
+    author='Aniket Bhattacharyea',
+    author_email='aniketmail669@gmail.com',
+    description='PEng is a templating engine inspired by Jinja2'
+)
